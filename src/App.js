0f2c8ad6f637be91ebaf2df02c9f34c9b89a118a
mohamed lahmer <mohamedlahmer600@gmail.com>
2021-02-05 22:27:52 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Button} from 'react-bootstrap';
 import './App.css';
+import tof from "./assets/pic.jpg"
 
-function App() {
+
+class App extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			interval: null,
+			timer: 0
+		};
+	}
+
+	componentDidMount(){
+		this.setState ({
+			interval: setInterval( () =>{
+				this.setState({timer:this.state.timer + 1});
+			},1000)
+		})
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.state.interval)
+	}
+
+    state={
+      isVisible:false
+    }
+
+    person=[
+      'Mohamed lahmer',
+      'Graduated in 2017',
+      'Water-ressources engineer'
+    ]
+  toggleVisibility = () => {
+    this.setState({
+      isVisible : !this.state.isVisible
+    })
+  }
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+			<div className="mb-2">
+			<h2 className='timer'>{this.state.timer}</h2>	
+      <Button variant="primary" size="sm" onClick={this.toggleVisibility}>{this.state.isVisible ? "Hide profile" : "Show profile" }
+			</Button>{' '}
+			</div>
+      {this.state.isVisible ? 
+    (
+    <div>
+      <ul className='list'>
+       {this.person.map((info,i)=> (
+         <li key={i}>{info}</li>
+         ))}
+      </ul>   
+      <img className='img' src={tof} alt='img' width='400px'></img>
+			
+    </div>
+		
+    ): (<h4>Click on the button to see the profile</h4>)}
     </div>
   );
 }
-
+}
 export default App;
